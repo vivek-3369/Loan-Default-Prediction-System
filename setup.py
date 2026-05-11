11a18b49d57edc4fdd84649cc3f5f10504b2d045
vivek-3369 <boddulvivek7474@gmail.com>
2026-05-11 12:27:34 +0530

    project structure updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vivek Boddul",
+    author_email="boddulvivek7474@gmail.com",
+    packages=find_packages()
+)
